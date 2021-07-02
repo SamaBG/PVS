02476169e296a41f424c7eff190cfa370c1690b1
SamaBG <86538476+SamaBG@users.noreply.github.com>
2021-07-02 00:14:40 -0300

    Update listado.php

--- listado.php
+++ listado.php
@@ -27,7 +27,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="img/log_barra.jpg" type="image/x-icon"/>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/estil.css" >
+  <link rel="stylesheet" href="css/estils.css" >
   
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -56,6 +56,7 @@
             <h2 style="text-align: left; font-family: inherit;color:#21618C">Listado Vehículos</h2>
                   
             <br><br>
+            <div class="table-responsive">
             <table id="tabla_plan "class="table table-striped">
                 <thead>
                 <tr>
@@ -105,6 +106,7 @@
              
             </tbody>
           </table>
+            </div>
          
       </div>
      <footer>
